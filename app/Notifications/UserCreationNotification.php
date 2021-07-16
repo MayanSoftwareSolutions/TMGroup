27a7a8c5e45a652b6f8f31d95c8789a27471050a
MayanSoftwareSolutions <86120293+MayanSoftwareSolutions@users.noreply.github.com>
2021-07-16 16:14:54 +0100

    changed welcome email

--- app/Notifications/UserCreationNotification.php
+++ app/Notifications/UserCreationNotification.php
@@ -29,7 +29,7 @@ class UserCreationNotification extends Notification
     {
         return (new MailMessage)
                     ->line('Welcome '.$this->user->name)
-                    ->line('This is our TMO management digital platform, the smart client management solution.')
+                    ->line('This is our TMGroups digital platform, the smart client management solution.')
                     ->line('An account has been created for you which can be accessed via the below link. Your login credential will be sent to you by an administrator shortly.')
                     ->line('Please ensure you change your password when you first login. This can be done by clicking the user icon and selecting My Profile.')
                     ->action('My Account', url('/login'));
